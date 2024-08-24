cc974ce35cad2963e413100a38c67e28b4cd922b
AyresMonteiro <main@ayresmonteiro.com>
2024-08-24 10:07:19 -0300

    feat: improve theme handling

--- src/js/theme.js
+++ src/js/theme.js
@@ -28,67 +28,32 @@ const themeNames = {
   honeycomb: 'Honeycomb',
 }
 
-const themeMainColors = {
-  blue: '#007ea7',
-  green: '#4caf50',
-  purple: '#9b59b6',
-  pale: '#5c8c8c',
-  honeycomb: '#ce8909',
-}
-
-const themeOptions = {
-  blue: './src/css/themes/blue.css',
-  green: './src/css/themes/green.css',
-  purple: './src/css/themes/purple.css',
-  pale: './src/css/themes/pale.css',
-  honeycomb: './src/css/themes/honeycomb.css',
-}
-
-const currentThemeElement = document.querySelector('#current-theme')
-
 /**
  * Set theme on the document.
  *
  * @param {string} theme
  */
 const setTheme = (theme) => {
-  const themePath = themeOptions[theme]
-  const themeElement = document.querySelector('#current-theme')
+  const rootElement = document.documentElement
 
-  themeElement.href = themePath
+  rootElement.setAttribute('data-theme', theme)
 
-  currentTheme = theme
   saveTheme(theme)
 }
 
-setTheme(currentTheme)
-
-const themeSelector = document.querySelector('#theme-selector')
-
-themeSelector.addEventListener('change', (event) => {
-  const theme = event.target.value
-  setTheme(theme)
-})
-
-const generateOptions = () => {
-  const options = Object.entries(themeOptions)
+/**
+ * Generate options for the theme selector.
+ * 
+ * @param {HTMLSelectElement} themeSelector 
+ */
+const generateOptions = (themeSelector) => {
+  const options = Object.entries(themeNames)
     .map(([theme, path]) => {
-      const themeMainColor = themeMainColors[theme]
-
       const optionElement = document.createElement('option')
-
-      const smallColoredCircleElement = document.createElement('span')
-
-      smallColoredCircleElement.style.display = 'inline-block'
-      smallColoredCircleElement.style.width = '10px'
-      smallColoredCircleElement.style.height = '10px'
-      smallColoredCircleElement.style.borderRadius = '50%'
-      smallColoredCircleElement.style.backgroundColor = themeMainColor
 
       optionElement.value = theme
       optionElement.selected = theme === currentTheme
-      optionElement.innerHTML = `${themeNames[theme]} `
-      optionElement.prepend(smallColoredCircleElement)
+      optionElement.innerHTML = themeNames[theme] + ' '.repeat(3)
 
       return optionElement.outerHTML
     })
@@ -97,5 +62,22 @@ const generateOptions = () => {
   themeSelector.innerHTML = options
 }
 
-generateOptions()
-themeSelector.value = currentTheme
+/**
+ * Setup theme selector.
+ *
+ * @param {string} initialTheme 
+ */
+const setupThemeSelector = (initialTheme) => {
+  const themeSelector = document.querySelector('#theme-selector')
+
+  themeSelector.addEventListener('change', (event) => {
+    const theme = event.target.value
+    setTheme(theme)
+  })
+
+  generateOptions(themeSelector)
+  themeSelector.value = initialTheme
+  setTheme(initialTheme)
+}
+
+setupThemeSelector(currentTheme)
